deea68a044f89afeed819bc07671f37ecd8d6f9c
Nuno Bispo <nuno.m.bispo@gmail.com>
2022-03-10 20:48:35 +0100

    Added logs to execute command

--- daemon/commands.py
+++ daemon/commands.py
@@ -1,12 +1,23 @@
+from sys import stdout
 from daemon.daemon import run_command
 import json
 
 
-def create_app(app_name):
-    command = f'apps:create {app_name}'
+def __execute_command(command):
+    stdout.write(f'Executing command: {command}')
     success, message = run_command(command)
+    stdout.write(f'Success: {success}')
+    stdout.write(f'Message: {message}')
     if success:
         json_output = json.loads(message)
-        success = True if json_output['ok'] else False
+        stdout.write(f'JSON: {json_output}')
+        success = True if json_output['ok'] == 'true' else False
         message = json_output['output']
+    return success, message
+
+
+# Creates an application
+def create_app(app_name):
+    command = f'apps:create {app_name}'
+    success, message = __execute_command(command)
     return success, message
