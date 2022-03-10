d5f751acb20d32871209433e68fb73efe810b643
Nuno Bispo <nuno.m.bispo@gmail.com>
2022-03-10 20:41:37 +0100

    Create application command

--- daemon/commands.py
+++ daemon/commands.py
@@ -1,8 +1,12 @@
 from daemon.daemon import run_command
+import json
 
 
 def create_app(app_name):
     command = f'apps:create {app_name}'
-    success, output = run_command(command)
-    return success, output
-
+    success, message = run_command(command)
+    if success:
+        json_output = json.loads(message)
+        success = True if json_output['ok'] else False
+        message = json_output['output']
+    return success, message
